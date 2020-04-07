f8ddd4d3a95cd970317608aa581407cf2d3e6bad
hassan kiwan <hassanhitleap1@gmail.com>
2020-04-07 16:55:00 +0300

    4/7/2020 (8)

--- views/site/contact.php
+++ views/site/contact.php
@@ -32,58 +32,72 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 </div>
 
-<div class="container">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
-    <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
-
+<!--contact us section start -->
+<div class="contact_us_section med_toppadder100 med_bottompadder70">
+    <div class="container">
         <div class="row">
-            <div class="col-lg-5">
+            <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+                <div class="choose_heading_wrapper med_bottompadder30">
+                    <h1 class="med_bottompadder20">Contact us</h1>
+                    <img src="<?= Yii::getAlias('@web')?>/theme/images/line.png" alt="title" class="med_bottompadder20">
+                </div>
+                <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+                <?php else: ?>
+                    <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="contect_form1 dc_cont_div">
+                            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="contect_form1 dc_cont_div">
+                            <?= $form->field($model, 'email') ?>
+                        </div>
+                    </div>
+                </div>
 
-                    <?= $form->field($model, 'email') ?>
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                            <div class="contect_form1 dc_cont_div">
+                                <?= $form->field($model, 'subject') ?>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                            <div class="contect_form1 dc_cont_div">
+                                <?= $form->field($model, 'phone') ?>
+                            </div>
+                        </div>
+                    </div>
 
-                    <?= $form->field($model, 'subject') ?>
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                            <div class="contect_form1 dc_cont_div">
+                                <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+                            </div>
+                        </div>
 
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+                    </div>
 
                     <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
+                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                ]) ?>
 
                     <div class="form-group">
                         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                     </div>
 
-                <?php ActiveForm::end(); ?>
+                    <?php ActiveForm::end(); ?>
 
+
+                <?php endif; ?>
             </div>
         </div>
 
-    <?php endif; ?>
+    </div>
 </div>
+
+
+
