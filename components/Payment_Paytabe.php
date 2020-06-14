e95bcb4bc3fb86d81090b211fc0da3b400f15ab4
hassan kiwan <hassanhitleap1@gmail.com>
2020-06-14 16:46:40 +0300

    pay tab

---
+++ components/Payment_Paytabe.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace  app\components;
+
+use Yii;
+use yii\base\BaseObject;
+
+
+class Payment_Paytabe extends BaseObject
+{
+    public static  function init()
+    {
+        # code...
+        
+    }
+
+
+}
+
+
+
+?>
